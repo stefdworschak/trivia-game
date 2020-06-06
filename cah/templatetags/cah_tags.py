c5f2b32c29f6a8f42430a3e6cd389dc9a8f56774
Stefan Dworschak <stef.dworschak@gmail.com>
2020-06-06 22:55:29 +0100

    Adjusting turn labels (#16)
    -) Changing the template tag to calculate the turn correctly

--- cah/templatetags/cah_tags.py
+++ cah/templatetags/cah_tags.py
@@ -13,20 +13,19 @@ def all_submissions(num_players, submissions):
 
 @register.simple_tag()
 def calculate_turn(**kwargs):
-    num_players = kwargs['r']
-    num_rounds = kwargs['pl']
+    num_rounds = kwargs['r']
+    num_players = kwargs['pl']
     if isinstance(num_players, str):
         num_players = int(num_players)
     if isinstance(num_rounds, str):
         num_rounds = int(num_rounds)
-    turn = (num_players * num_rounds) % num_players
+    turn = (num_rounds) % num_players
     return num_players if turn == 0 else turn
 
 @register.simple_tag()
 def calculate_round(**kwargs):
     num_rounds = kwargs['r']
     current_round = kwargs['cr']
-
     if isinstance(num_rounds, str):
         num_rounds = int(num_rounds)
     if isinstance(current_round, str):
